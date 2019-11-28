851c973cad744e01c6a0cdccbbb083f629ff3fcb
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-28 16:52:42 +0300

    create a login.html to help users login to the blog

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,4 @@
-from wtf import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
--- app/auth/views.py
+++ app/auth/views.py
@@ -5,15 +5,16 @@ from .. import db
 from . import auth
 from flask_login import login_user
 
-@auth.route('/login',['GET','POST'])
+@auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data)
-        login_user(user,login_form.remember.data)
-        return redirect(url_for('main.index'))
-        flash(f'Invalid username or Password')
+        if user is not None and user.verify_password(login_form.password.data):
+            
+            login_user(user,login_form.remember.data)
+            return redirect(url_for('main.index'))
+            flash(f'Invalid username or Password')
     
     title = 'Nyakinyua Blog login'
     return render_template('auth/login.html', login_form=login_form,title=title)
