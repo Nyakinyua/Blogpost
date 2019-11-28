9a2c6037b74abcddefcd3510cefb30b81a0f688c
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-28 21:24:18 +0300

    create a logout route that calls the flask_logins logout_user function

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from . import auth
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -31,4 +31,10 @@ def register():
         title = 'New Account'
         
     return render_template('auth/register.html', registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
 
