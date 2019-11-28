e424be445275ca0021d0fab62ba3c4e855c1f96b
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-28 13:26:42 +0300

    create class user and define the objects in class user

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 class Quote:
     '''
     Quote class to define the quote objects
@@ -7,4 +10,18 @@ class Quote:
         self.author=author
         self.quote=quote
         
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(),unique = True,index = True)
+    pass_word = db.Column(db.String(255))
+    
+    
+    @property
+    def
+
+    def __repr__(self):
+        return f'User {self.username}'        
         
