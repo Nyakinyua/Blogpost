63820afb6564e81a607b90180db2d2e50800c1c2
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-29 09:17:26 +0300

    add new columns to user table to update user profile

--- app/models.py
+++ app/models.py
@@ -19,6 +19,8 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pass_word = db.Column(db.String(255))
     
     
