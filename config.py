d0eb7e68872fb91f3cbfa08110f72f3cf3683962
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-29 08:24:16 +0300

    add bootstrap to the layout.html file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'sd2dfghj3457vberdfg56'
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nyakinyua:Lastman@localhost/blog'
    
